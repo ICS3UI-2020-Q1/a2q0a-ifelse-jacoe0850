e2451d27ad68e2c07e5882f4a35b4ee289238306
jacoe0850 <jacoe0850@wrdsb.ca>
2020-09-14 16:14:35 +0000

    e

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+        // store how old you must be to get your license
+        final int LICENSE_AGE = 16;
+    // get the user's age and store it for later
+    System.out.println("Please enter your age:");
+    int age = input.nextInt();
+      // check to see if they are old enough to get a license
+if (age >= 16){
+   System.out.println("You can get your driver's license!");
+  }else{System.out.println("Sorry, you are not old enough to drive.");
+    }
+}
 }
